319790b7db5be7c0130d7a1dac47f20abee647f7
Devendra <ingale.devendra20@gmail.com>
2024-07-22 09:45:33 +0530

    main

--- EC2-Launch.tf
+++ EC2-Launch.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = ""
-  secret_key = ""
-}
-
 resource "aws_instance" "Terraform-ec2" {
     ami = "ami-0645cf88151eb2007"
     instance_type = "t2.micro"
